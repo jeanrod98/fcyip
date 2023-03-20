f7d86788e008bf5d44bd2d3d6815113c1fbcc58d
jeanrod98 <jcrod.jc@gmail.com>
2023-03-20 12:27:07 -0500

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Imagen from "./assets/Yip.png"
+import Spinner from './Components/Spinner';
 
 function App() {
+
+  const [ spinner, setSpinner ] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSpinner(false)
+    }, 3000);
+
+  }, [])
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>¡FELIZ CUMPLEAÑOS YIPSY!&#127881;</h1>
       </header>
+      <main>
+        <div className="imagen">
+          <img src={Imagen} alt=""/>
+        </div>
+
+        <div className="texto">
+          <p>"No hay nada tan lindo en la vida, como encontrar personas como tú, que saben hacer de pequeños instantes, grandes momentos. Gracias por aparecerte en el camino de mi hermano y de mi familia."</p>
+        </div>
+
+      </main>
+      
     </div>
+    {spinner &&
+      <Spinner/>
+    }
+    </>
   );
 }
 
---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className='modal-spinner'>
+        <span>Y</span>
+        <div class="spinner">
+           
+        </div> 
+    </div>
+  )
+}
+
+export default Spinner
